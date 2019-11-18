d9db127407d998ceae22520e8042ac5ff9a539c1
gp16860 <57736919+gp16860@users.noreply.github.com>
2019-11-18 13:20:29 +0800

    Update kubernets-cluster.sh

--- kubernets-cluster.sh
+++ kubernets-cluster.sh
@@ -6,20 +6,67 @@ yum install -y conntrack ipvsadm ipset jq sysstat curl iptables libseccomp wget 
 
 sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 
+yum install -y docker-ce-18.09.0 docker-ce-cli-18.09.0 containerd.io
+
+sudo systemctl start docker && sudo systemctl enable docker
+
+# 关闭防火墙
+systemctl stop firewalld && systemctl disable firewalld
+
+# 关闭selinux
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# 关闭swap
+swapoff -a
+sed -i '/swap/s/^\(.*\)$/#\1/g' /etc/fstab
+
+# 配置iptables的ACCEPT规则
+iptables -F && iptables -X && iptables -F -t nat && iptables -X -t nat && iptables -P FORWARD ACCEPT
+
+# 设置系统参数
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+#应用配置
+sysctl --system
+
+#配置yum源
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=http://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=0
+repo_gpgcheck=0
+gpgkey=http://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg
+       http://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+
+yum install -y kubeadm-1.14.0-0 kubelet-1.14.0-0 kubectl-1.14.0-0
+
 sudo mkdir -p /etc/docker
 
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
-  "registry-mirrors": ["这边替换成自己的实际地址"]  #需要修改
+  "registry-mirrors": ["替换成自己的实际地址"]  #需要修改
 }
 EOF
 
 sudo systemctl daemon-reload
 
-yum install -y docker-ce-18.09.0 docker-ce-cli-18.09.0 containerd.io
+systemctl restart docker
 
-sudo systemctl start docker && sudo systemctl enable docker
+sed -i "s/cgroup-driver=systemd/cgroup-driver=cgroupfs/g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+	
+systemctl enable kubelet && systemctl start kubelet
+
+sh ./kubeadm.sh
+
+
 
 
 
